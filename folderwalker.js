4ea05c70fee74ca2d35089f0795459f16865c6fe
james wolf <contractorwolf@hotmail.com>
2020-07-16 11:06:49 -0400

    initial commit

---
+++ folderwalker.js
@@ -0,0 +1,29 @@
+  
+var fs = require('fs');
+
+console.log("requires loaded");
+
+var WalkFolder = function(folder, fileext) {
+
+    console.log(`checking directory: ${folder}`);  
+    
+    fs.readdir(folder, (err, records) => {
+        records.forEach(function(record){
+            console.log(`found: ${folder}/${record}`);                    
+            fs.stat(`${folder}/${record}`, function(err, stat) {
+                if (stat && stat.isDirectory()) {
+                    WalkFolder(`${folder}/${record}`, fileext);
+                } else {                    
+                    if(record.endsWith(fileext)){
+                        fs.unlinkSync(`${folder}/${record}`);
+                        console.log(`[ DELETED: ${folder}/${record} ] ---------------------------------------------------------------------------<<<`);                                
+                    }
+                }
+            });
+        });
+    });
+};
+
+var foldername = process.argv.slice(2)
+
+WalkFolder(`./${foldername}`,'.map');
